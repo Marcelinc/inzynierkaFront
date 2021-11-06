3f0546854eae6a4828fcd0d8d7ec4190e14c0642
Marcelinc <marcin.c40@wp.pl>
2021-11-06 14:46:31 +0100

    Szablon zakładki 'Moje zlecenia'

--- src/Components/OwnOrders.js
+++ src/Components/OwnOrders.js
@@ -8,20 +8,53 @@ const OwnOrders = () => {
     const history = useHistory();
 
     return(<section className='data'>
-        {dataType === 'list' && <div className='equipment-list'>
-            <p>Moje zlecenia</p>
+        {dataType === 'list' && <div className='equipment-content'>
+            <h3>Moje zlecenia</h3>
+            <div className='legend' id='ordersLegend'>
+                <span>Nazwa</span>
+                <span>Przewidywany czas rozpoczęcia</span>
+                <span>Status</span>
+            </div>
             <div id='myOrders'>
                 <div className='mOrder unit' onClick={() => {setType('order'); history.push(`/user/zlecenia/${1}`)}}>
                     <span>Nazwa zlecenia</span>
                     <span>Przewidywany czas rozpoczęcia</span>
+                    <span>Zakończone</span>
                 </div>
                 <div className='mOrder unit' onClick={() => setType('order')}>
                     <span>Nazwa zlecenia</span>
                     <span>Przewidywany czas rozpoczęcia</span>
+                    <span>Zakończone</span>
                 </div>
                 <div className='mOrder unit' onClick={() => setType('order')}>
                     <span>Nazwa zlecenia</span>
                     <span>Przewidywany czas rozpoczęcia</span>
+                    <span>Zakończone</span>
+                </div>
+                <div className='mOrder unit' onClick={() => setType('order')}>
+                    <span>Nazwa zlecenia</span>
+                    <span>Przewidywany czas rozpoczęcia</span>
+                    <span>Zakończone</span>
+                </div>
+                <div className='mOrder unit' onClick={() => setType('order')}>
+                    <span>Nazwa zlecenia</span>
+                    <span>Przewidywany czas rozpoczęcia</span>
+                    <span>Zakończone</span>
+                </div>
+                <div className='mOrder unit' onClick={() => setType('order')}>
+                    <span>Nazwa zlecenia</span>
+                    <span>Przewidywany czas rozpoczęcia</span>
+                    <span>Przerwane</span>
+                </div>
+                <div className='mOrder unit' onClick={() => setType('order')}>
+                    <span>Nazwa zlecenia</span>
+                    <span>Przewidywany czas rozpoczęcia</span>
+                    <span>Przyjęte</span>
+                </div>
+                <div className='mOrder unit' onClick={() => setType('order')}>
+                    <span>Nazwa zlecenia</span>
+                    <span>Przewidywany czas rozpoczęcia</span>
+                    <span>Zakończone</span>
                 </div>
             </div>
         </div>}
